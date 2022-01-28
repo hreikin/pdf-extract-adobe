d7798da2d1747ca90456f2f02846f8c88ee8302b
Michael Haslam <hreikin@gmail.com>
2022-01-28 21:14:18 +0000

    List all functions created in extraction.py for testing.

--- build/main.py
+++ build/main.py
@@ -4,5 +4,18 @@ import os
 source_path = "../test/"
 for root, dirnames, filenames in os.walk(source_path):
     for filename in filenames:
+        # All functions listed below work and currently output a zip file with 
+        # the relevant data inside.
         extraction.extract_txt_from_pdf(filename)
+        # extraction.extract_txt_from_pdf_with_custom_timeouts(filename)
+        # extraction.extract_txt_table_info_from_pdf(filename)
+        # extraction.extract_txt_table_info_with_char_bounds_from_pdf(filename)
+        # extraction.extract_txt_table_info_with_figure_tables_rendition_from_pdf(filename)
+        # extraction.extract_txt_table_info_with_rendition_from_pdf(filename)
+        # extraction.extract_txt_table_info_with_table_structure_from_pdf(filename)
+        # extraction.extract_txt_with_char_bounds_from_pdf(filename)
+        # extraction.extract_txt_with_styling_info_from_pdf(filename)
 
+
+        # Not currently working
+        # extraction.extract_txt_from_pdf_with_in_memory_auth_credentials(filename)
