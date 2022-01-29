dd291d6216f35f2d64331f1391e402038bb9277e
Michael Haslam <hreikin@gmail.com>
2022-01-29 08:50:57 +0000

    Extract JsonSchema from zip files.

--- build/main.py
+++ build/main.py
@@ -1,12 +1,12 @@
 import extraction
-import os
+import os, zipfile
 
 source_path = "../test/"
-for root, dirnames, filenames in os.walk(source_path):
-    for filename in filenames:
+# for root, dirnames, filenames in os.walk(source_path):
+    # for filename in filenames:
         # All functions listed below work and currently output a zip file with 
         # the relevant data inside.
-        extraction.extract_all_from_pdf(filename)
+        # extraction.extract_all_from_pdf(filename)
         # extraction.extract_txt_from_pdf(filename)
         # extraction.extract_txt_from_pdf_with_custom_timeouts(filename)
         # extraction.extract_txt_table_info_from_pdf(filename)
@@ -21,4 +21,13 @@ for root, dirnames, filenames in os.walk(source_path):
         # Not currently working
         # extraction.extract_txt_from_pdf_with_in_memory_auth_credentials(filename)
 
-json_source = "../output/"
+json_source = os.path.realpath("../output/")
+os.chdir(json_source)
+for file in os.listdir(json_source):
+    if zipfile.is_zipfile(file):
+        dirname = file.rstrip(".zip")
+        output_path = os.path.join(json_source + "/" + dirname)
+        if not os.path.isdir(output_path):
+            os.mkdir(output_path)
+        with zipfile.ZipFile(file) as item:
+            item.extractall(output_path)
