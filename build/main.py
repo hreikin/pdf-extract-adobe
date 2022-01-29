b80725d13558ce623fee02f804c221197e998cf4
Michael Haslam <hreikin@gmail.com>
2022-01-29 15:29:18 +0000

    Formatting and comments.

--- build/main.py
+++ build/main.py
@@ -2,23 +2,25 @@ import json
 import extraction
 import os, zipfile
 
-# source_path = "../test/"
-# for root, dirnames, filenames in os.walk(source_path):
-#     for filename in filenames:
-#         extraction.extract_all_from_pdf(filename)
+# Creates Json Schema zip file with Adobe API.
+source_path = "../test/"
+for root, dirnames, filenames in os.walk(source_path):
+    for filename in filenames:
+        extraction.extract_all_from_pdf(filename)
 
+# Extracts Json Schema from zip file.
+json_source = os.path.realpath("../test/")
+os.chdir(json_source)
+for file in os.listdir(json_source):
+    if zipfile.is_zipfile(file):
+        dirname = file.rstrip(".zip")
+        output_path = os.path.join(json_source + "/json/" + dirname)
+        if not os.path.isdir(output_path):
+            os.makedirs(output_path, exist_ok=True)
+        with zipfile.ZipFile(file) as item:
+            item.extractall(output_path)
 
-# json_source = os.path.realpath("../test/")
-# os.chdir(json_source)
-# for file in os.listdir(json_source):
-#     if zipfile.is_zipfile(file):
-#         dirname = file.rstrip(".zip")
-#         output_path = os.path.join(json_source + "/json/" + dirname)
-#         if not os.path.isdir(output_path):
-#             os.makedirs(output_path, exist_ok=True)
-#         with zipfile.ZipFile(file) as item:
-#             item.extractall(output_path)
-
+# Loads Json Schema and prints the output.
 schema_source = os.path.realpath("../test/json/")
 for root, dirnames, filenames in os.walk(schema_source):
     for filename in filenames:
