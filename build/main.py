4c93d1b6c6eb0bff4ce5debc0191f8c33b29b1aa
Michael Haslam <hreikin@gmail.com>
2022-02-06 11:47:07 +0000

    Fix names.

--- build/main.py
+++ build/main.py
@@ -1,4 +1,4 @@
-import extraction, confidence, process_pdf
+import extraction, confidence, processing
 import logging
 
 ##################################### LOGS #####################################
@@ -25,42 +25,46 @@ logging.getLogger('').addHandler(console)
 
 ################################################################################
 
-# # Creates Json Schema zip file with Adobe API.
-# source_path = "../test/pdfs/"
-# logging.info("Creating JSON Schema zip files with Adobe API.")
-# extraction.extract_pdf_adobe(source_path)
+# Creates Json Schema zip file with Adobe API.
+source_path = "../test/pdfs/"
+logging.info("Creating JSON Schema zip files with Adobe API.")
+extraction.extract_pdf_adobe(source_path)
 
-# # Extracts Json Schema from zip file.
-# zip_source = "../test/json-zips/"
-# json_output = "../test/json-schema/"
-# logging.info("Extracting JSON Schema from zip files.")
-# extraction.extract_json_from_zip(zip_source, json_output)
+# Extracts Json Schema from zip file.
+zip_source = "../test/json-zips/"
+json_output = "../test/json-schema/"
+logging.info("Extracting JSON Schema from zip files.")
+extraction.extract_from_zip(zip_source, json_output)
 
-# # Targets "Text" entries from the Json Schema and adds them to a file for each pdf.
-# schema_source = "../test/json-schema/"
-# output_path = "../test/confidence-check/"
-# logging.info("Targeting 'Text' entries from the JSON Schema.")
-# confidence.extract_text_from_json(schema_source, output_path)
+# Targets "Text" entries from the Json Schema and adds them to a file for each pdf.
+schema_source = "../test/json-schema/"
+output_path = "../test/confidence-check/"
+logging.info("Targeting 'Text' entries from the JSON Schema.")
+confidence.extract_text_from_json(schema_source, output_path)
 
-# # Convert PDF files to images for OCR/accuracy check.
-# pdf_path = "../test/pdfs/"
-# image_path = "../test/confidence-check/converted-images/"
-# image_format = "png"
-# logging.info("Converting PDF files to images.")
-# confidence.convert_pdf_to_image(pdf_path, image_path, image_format)
+# Convert PDF files to images for OCR/accuracy check.
+pdf_path = "../test/pdfs/"
+image_path = "../test/confidence-check/converted-images/"
+image_format = "png"
+logging.info("Converting PDF files to images.")
+confidence.split_all_pages_image(pdf_path, image_path, image_format)
 
-# # Run the converted pdf images through OCR and create a text file for each one as output.
-# input_path = "../test/confidence-check/"
-# output_path = "../test/confidence-check/"
-# image_format = "png"
-# logging.info("Running the converted images through OCR.")
-# confidence.ocr_converted_pdf_images(input_path, output_path, image_format)
+# Run the converted pdf images through OCR and create a text file for each one as output.
+input_path = "../test/confidence-check/"
+output_path = "../test/confidence-check/"
+image_format = "png"
+logging.info("Running the converted images through OCR.")
+confidence.ocr_images(input_path, output_path, image_format)
 
-# # Confidence Check
-# input_path = "../test/confidence-check/"
-# logging.info("Performing basic confidence check.")
-# confidence.confidence_check_text(input_path)
-# logging.info("Process complete, exiting.")
+# Confidence Check
+input_path = "../test/confidence-check/"
+logging.info("Performing basic confidence check.")
+confidence.confidence_check_text(input_path)
+logging.info("Process complete, exiting.")
 
 pdf_file = "../test/pdfs/gce-core-rules-1.21.pdf"
-process_pdf.split_all_pages(pdf_file)
+processing.split_all_pages_pdf(pdf_file)
+
+file_one = "../test/pdfs/Daresbury_labs_CS.1.pdf"
+file_two = "../test/pdfs/Sputtering-Targets.pdf"
+processing.append_pdf(file_one, file_two)
