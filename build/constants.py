9669650d863a37ce2848a1bd773a077fd2de2e24
Michael Haslam <hreikin@gmail.com>
2022-02-15 21:43:44 +0000

    Constants file to keep various variables which are needed in multiple functions.

---
+++ build/constants.py
@@ -0,0 +1,12 @@
+headings = ["Title"]
+paragraphs = ["P", "LBody", "ParagraphSpan", "Span", "StyleSpan"]
+lists = ["L"]
+table_rows = ["TR"]
+figures = ["Figure", "Table"]
+for i in range(0, 101):
+    headings.append(f"H{i}")
+    paragraphs.append(f"P[{i}]")
+    lists.append(f"LI[{i}]")
+    table_rows.append(f"TR[{i}]")
+    figures.append(f"Table[{i}]")
+    figures.append(f"Figure[{i}]")
