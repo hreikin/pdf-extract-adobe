9842f535c4da5819b542848083361c8201e625e1
Michael Haslam <hreikin@gmail.com>
2022-02-17 21:17:19 +0000

    Add new constants for common directories and unwanted string items.

--- build/constants.py
+++ build/constants.py
@@ -1,13 +1,21 @@
 from pathlib import Path
-# src_dir = Path("../src/").resolve()
-src_dir = Path("../test/").resolve()
+
 base_dir = Path("..").resolve()
-database = "../test/sqlite/pcc.sqlite"
+src_dir = base_dir / "test"
+confidence_dir = src_dir / "confidence"
+converted_dir = src_dir / "converted"
+created_dir = src_dir / "created"
+extracted_dir = src_dir / "extracted"
+json_dir = src_dir / "json"
+pdf_dir = src_dir / "pdfs"
+zip_dir = src_dir / "zips"
+database = src_dir / "sqlite" / "pcc.sqlite"
 headings = ["Title"]
 paragraphs = ["P", "LBody", "ParagraphSpan", "Span", "StyleSpan"]
 lists = ["L"]
 table_rows = ["TR"]
 figures = ["Figure", "Table"]
+unwanted_pdf = ["*", "◈", "-", "•", "* ", "◈ ", "- ", "• "]
 for i in range(0, 101):
     headings.append(f"H{i}")
     paragraphs.append(f"P[{i}]")
@@ -15,5 +23,3 @@ for i in range(0, 101):
     table_rows.append(f"TR[{i}]")
     figures.append(f"Table[{i}]")
     figures.append(f"Figure[{i}]")
-
-unwanted_pdf = ["*", "◈", "-", "•", "* ", "◈ ", "- ", "• "]
