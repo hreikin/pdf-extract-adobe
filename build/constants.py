0e42af956b87261728bbb5f1570c06dde585de14
Michael Haslam <hreikin@gmail.com>
2022-02-19 21:36:46 +0000

    Append more headings to constants and change paths to using "src".

--- build/constants.py
+++ build/constants.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 base_dir = Path("..").resolve()
-src_dir = base_dir / "test"
+src_dir = base_dir / "src"
 confidence_dir = src_dir / "confidence"
 converted_dir = src_dir / "converted"
 created_dir = src_dir / "created"
@@ -10,13 +10,13 @@ json_dir = src_dir / "json"
 pdf_dir = src_dir / "pdfs"
 zip_dir = src_dir / "zips"
 database = src_dir / "sqlite" / "pcc.sqlite"
-headings = ["Title"]
+headings = []
 paragraphs = ["P", "LBody", "ParagraphSpan", "Span", "StyleSpan"]
 lists = ["L"]
 table_rows = ["TR"]
 figures = ["Figure", "Table"]
 unwanted_pdf = ["*", "◈", "-", "•", "* ", "◈ ", "- ", "• "]
-for i in range(0, 101):
+for i in range(0, 201):
     headings.append(f"H{i}")
     paragraphs.append(f"P[{i}]")
     lists.append(f"LI[{i}]")
--- build/main.py
+++ build/main.py
@@ -1,5 +1,7 @@
-import adobe_json, confidence, extraction, processing, json_to_sqlite, utilities, convert
+import adobe_json, confidence, extraction, processing, json_to_sqlite, utilities, convert, constants
 import logging
+
+from pathlib import Path
 
 ##################################### LOGS #####################################
 # Initialize the logger and specify the level of logging. This will log "DEBUG" 
@@ -25,16 +27,27 @@ logging.getLogger('').addHandler(console)
 
 ################################################################################
 
-# Creates Json Schema zip file with Adobe API.
-source_path = "../test/pdfs/"
-logging.info("Creating JSON Schema with Adobe API.")
-adobe_json.extract_pdf_adobe(source_path)
 
-# # Extracts Json Schema from zip file. Automatically called after the API 
-# # requests are complete. Can be used on its own.
-# zip_source = "../test/zips/"
-# logging.info("Extracting JSON Schema from zip files.")
-# utilities.extract_from_zip(zip_source)
+new_headings = ["Title"]
+for item in constants.headings:
+    new_headings.append(item)
+    for i in range(0, 101):
+        new_headings.append(f"{item}[{i}]")
+
+constants.headings = new_headings
+
+
+
+# # Creates Json Schema zip file with Adobe API.
+# source_path = "../src/pdfs/"
+# logging.info("Creating JSON Schema with Adobe API.")
+# adobe_json.extract_pdf_adobe(source_path)
+
+# Extracts Json Schema from zip file. Automatically called after the API 
+# requests are complete. Can be used on its own.
+zip_source = "../src/zips/"
+logging.info("Extracting JSON Schema from zip files.")
+utilities.extract_from_zip(zip_source)
 
 # Creates list of PDF/URL pairs.
 logging.info("Creating PDF/URL list.")
@@ -43,68 +56,73 @@ adobe_json.create_pdf_url_list()
 # Splits the main Json Schema file into parts based on the top-level keys in the 
 # file and then targets "Text", "filePaths", "Path" and "Page" values from 
 # within the "elements.json" file and inserts it into an SQLite DB.
-src = "../test/json/"
+src = "../src/json/"
 logging.info("Manipulating Json and creating SQLite tables.")
 json_to_sqlite.split_main_json_file(src)
 
-# Takes data from SQLite DB and applies markdown formatting to it before writing 
-# to a file.
-path = "../test/json/Beam-Profile-Measuring-System/"
-convert.convert_db_markdown(path)
-path = "../test/json/Daresbury_labs_CS.1/"
-convert.convert_db_markdown(path, with_imgs=False)
-path = "../test/json/Sputtering-Targets/"
-convert.convert_db_markdown(path)
-path = "../test/json/Starter-Kit-Princeton-Scientific/"
-convert.convert_db_markdown(path)
-path = "../test/json/WS22/"
-convert.convert_db_markdown(path)
+src_dir = Path(constants.json_dir).resolve()
+for directory in src_dir.iterdir():
+    convert.convert_db_markdown(directory.resolve())
+
+
+# # Takes data from SQLite DB and applies markdown formatting to it before writing 
+# # to a file.
+# path = "../src/json/Beam-Profile-Measuring-System/"
+# convert.convert_db_markdown(path)
+# path = "../src/json/Daresbury_labs_CS.1/"
+# convert.convert_db_markdown(path)
+# path = "../src/json/Sputtering-Targets/"
+# convert.convert_db_markdown(path)
+# path = "../src/json/Starter-Kit-Princeton-Scientific/"
+# convert.convert_db_markdown(path)
+# path = "../src/json/WS22/"
+# convert.convert_db_markdown(path)
 
 # Convert PDF files to images for OCR/accuracy check.
-pdf_path = "../test/pdfs/"
+pdf_path = "../src/pdfs/"
 image_format = ".png"
 logging.info("Converting PDF files to images.")
 extraction.convert_pages_into_image(pdf_path, image_format)
 
 # Run the converted pdf images through OCR and create a text file for each one as output.
-input_path = "../test/converted/"
+input_path = "../src/converted/"
 logging.info("Running the converted images through OCR.")
 extraction.ocr_images_for_text_confidence(input_path)
 
 # Extract text from PDF with PyMuPDF.
-pdf_path = "../test/pdfs/"
+pdf_path = "../src/pdfs/"
 extraction.extract_text_from_pdf_confidence(pdf_path)
 
 # Confidence Check
-input_path = "../test/confidence/"
+input_path = "../src/confidence/"
 logging.info("Performing basic confidence check.")
 confidence.confidence_check_text(input_path)
 
 # Extract images from PDF
-pdf_path = "../test/pdfs/"
+pdf_path = "../src/pdfs/"
 extraction.extract_images_from_pdf(pdf_path)
 logging.info("Process complete, exiting.")
 
 # # Split all PDF pages
-# pdf_file = "../test/pdfs/Daresbury_labs_CS.1.pdf"
+# pdf_file = "../src/pdfs/Daresbury_labs_CS.1.pdf"
 # processing.split_all_pages_pdf(pdf_file)
 
 # # Merge two PDF files.
-# file_one = "../test/pdfs/Daresbury_labs_CS.1.pdf"
-# file_two = "../test/pdfs/Sputtering-Targets.pdf"
+# file_one = "../src/pdfs/Daresbury_labs_CS.1.pdf"
+# file_two = "../src/pdfs/Sputtering-Targets.pdf"
 # processing.append_pdf(file_one, file_two)
 
 # # Overlay PDF with another.
-# file_one = "../test/pdfs/Daresbury_labs_CS.1.pdf"
-# file_two = "../test/pdfs/Sputtering-Targets.pdf"
+# file_one = "../src/pdfs/Daresbury_labs_CS.1.pdf"
+# file_two = "../src/pdfs/Sputtering-Targets.pdf"
 # processing.overlay(file_one, file_two)
 
 # # Extract tables from PDF - MANUAL ONLY, might be a way to automate with a list.
-# pdf_path = "../test/pdfs/Sputtering-Targets.pdf"
+# pdf_path = "../src/pdfs/Sputtering-Targets.pdf"
 # start = "Metals "
 # end = "mmmmmmmm"
 # extraction.extract_tables_from_pdf(pdf_path, start, end)
-# pdf_path = "../test/pdfs/Sputtering-Targets.pdf"
+# pdf_path = "../src/pdfs/Sputtering-Targets.pdf"
 # start = " "
 # end = "Other materials"
 # page_num = 1
