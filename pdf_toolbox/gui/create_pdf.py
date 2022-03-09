a4fdb152ffa9444248428c709b2476ecd6775b35
Michael Haslam <hreikin@gmail.com>
2022-03-09 20:43:22 +0000

    Uses pygments to apply syntax highlighting to the markdown editor. The load_style function applies the monokai theme from pygments to the syntax highlighted text. The check_markdown functions checks/applies markdown as the user types. Extends the MarkdownLexer to apply bold-italic text (and others later).

--- pdf_toolbox/gui/create_pdf.py
+++ pdf_toolbox/gui/create_pdf.py
@@ -6,6 +6,11 @@ from tkinter import *
 from tkinter import font
 from tkinterweb import HtmlFrame
 from tkinter.scrolledtext import ScrolledText
+from pygments import lex
+from pygments.lexers.markup import MarkdownLexer
+from pygments.token import Generic
+from pygments.lexer import bygroups
+from pygments.styles import get_style_by_name
 
 class CreatePDF(Frame):
     def __init__(self, master=None):
@@ -43,6 +48,8 @@ class CreatePDF(Frame):
             default_text = stream.read()
         self.text_area.insert(0.0, default_text)
         self.text_area.bind("<<Modified>>", self.on_input_change)
+        # bind each key Release to the markdown checker function
+        self.text_area.bind("<KeyRelease>", lambda event: self.check_markdown())
         self.editor_frame.pack(fill="both", expand=1)
 
         self.preview_frame = HtmlFrame(self.editor_pw, relief="groove", borderwidth=5)
@@ -59,6 +66,8 @@ class CreatePDF(Frame):
         self.main_pw.add(self.create_options)
         self.main_pw.pack(fill="both", expand=1)
 
+        self.lexer = Lexer()
+        self.syntax_highlighting_tags = self.load_style("monokai")
 
     def on_input_change(self, event):
         """This is currently unfinished."""
@@ -67,3 +76,54 @@ class CreatePDF(Frame):
         markdownText = self.text_area.get("1.0", END)
         html = md2html.convert(markdownText)
         self.preview_frame.load_html(html)
+        self.check_markdown()
+
+    def load_style(self, stylename):
+        self.style = get_style_by_name(stylename)
+        self.syntax_highlighting_tags = []
+        for token, opts in self.style.list_styles():
+            kwargs = {}
+            fg = opts['color']
+            bg = opts['bgcolor']
+            if fg:
+                kwargs['foreground'] = '#' + fg
+            if bg:
+                kwargs['background'] = '#' + bg
+            font = ('Monospace', 10) + tuple(key for key in ('bold', 'italic') if opts[key])
+            kwargs['font'] = font
+            kwargs['underline'] = opts['underline']
+            self.text_area.tag_configure(str(token), **kwargs)
+            self.syntax_highlighting_tags.append(str(token))
+        self.text_area.configure(bg=self.style.background_color,
+                        fg=self.text_area.tag_cget("Token.Text", "foreground"),
+                        selectbackground=self.style.highlight_color)
+        self.text_area.tag_configure(str(Generic.StrongEmph), font=('Monospace', 10, 'bold', 'italic'))
+        self.syntax_highlighting_tags.append(str(Generic.StrongEmph))
+        return self.syntax_highlighting_tags    
+
+    def check_markdown(self, start='insert linestart', end='insert lineend'):
+        self.data = self.text_area.get(start, end)
+        while self.data and self.data[0] == '\n':
+            start = self.text_area.index('%s+1c' % start)
+            self.data = self.data[1:]
+        self.text_area.mark_set('range_start', start)
+        # clear tags
+        for t in self.syntax_highlighting_tags:
+            self.text_area.tag_remove(t, start, "range_start +%ic" % len(self.data))
+        # parse text
+        for token, content in lex(self.data, self.lexer):
+            self.text_area.mark_set("range_end", "range_start + %ic" % len(content))
+            for t in token.split():
+                self.text_area.tag_add(str(t), "range_start", "range_end")
+            self.text_area.mark_set("range_start", "range_end")
+
+
+# Extend MarkdownLexer to add markup for bold-italic
+class Lexer(MarkdownLexer):
+    tokens = {key: val.copy() for key, val in MarkdownLexer.tokens.items()}
+    # # bold-italic fenced by '***'
+    tokens['inline'].insert(2, (r'(\*\*\*[^* \n][^*\n]*\*\*\*)',
+                                bygroups(Generic.StrongEmph)))
+    # # bold-italic fenced by '___'
+    tokens['inline'].insert(2, (r'(\_\_\_[^_ \n][^_\n]*\_\_\_)',
+                                bygroups(Generic.StrongEmph)))
