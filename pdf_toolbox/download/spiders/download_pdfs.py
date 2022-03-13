6b50158d6adb1b69a76204ae6d10e172636de649
Michael Haslam <hreikin@gmail.com>
2022-03-13 13:01:55 +0000

    Function to run spider from GUI/script.

--- pdf_toolbox/download/spiders/download_pdfs.py
+++ pdf_toolbox/download/spiders/download_pdfs.py
@@ -1,11 +1,12 @@
-from urllib.request import Request
-import scrapy
+import logging, scrapy
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
 from download.items import DownloadFilesItem
 
 class DownloadPdfsSpider(scrapy.Spider):
     name = 'download_pdfs'
-    allowed_domains = ['princetonscientific.com']
-    start_urls = ['https://princetonscientific.com/']
+    allowed_domains = []
+    start_urls = []
 
     def parse(self, response):
       for href in response.css("a::attr('href')"):
@@ -24,3 +25,9 @@ class DownloadPdfsSpider(scrapy.Spider):
                 item['original_filename'] = url.split("/")[-1]
                 item['page'] = response.url
                 yield item
+
+def run_spider(allowed_domain, start_url):
+    spider_process = CrawlerProcess(get_project_settings())
+    spider_process.crawl(DownloadPdfsSpider, start_urls=[start_url], allowed_domains=[allowed_domain])
+    spider_process.start()
+    logging.info("Crawling finished.")
